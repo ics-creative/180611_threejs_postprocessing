1693fc2c7ce5295abe6222b5665f12dbfa0f54e5
ics-nohara <nozomi.nohara@ics-web.jp>
2015-03-12 21:27:11 +0900

    gulp：動作不具合の修正(cssがうまくコピーできていなかった)

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,25 @@ var uglify = require('gulp-uglify');
 
 browserSync({
     server: {
-        baseDir: "build" 
+        baseDir: "build"
     }
 });
-gulp.task('build-minify',function() {
+gulp.task('build-minify', function () {
     gulp.src(['src/ts/**/*.ts'])
         .pipe(typescript({
             target: "ES5",
             removeComments: true,
             sortOutput: true
         }))
-    .js
-    .pipe(concat("main.js"))
-    .pipe(uglify({
-        preserveComments:'some'
-    }))
-    .pipe(gulp.dest('build/js/'));
+        .js
+        .pipe(concat("main.js"))
+        .pipe(uglify({
+            preserveComments: 'some'
+        }))
+        .pipe(gulp.dest('build/js/'));
 });
 
-gulp.task('typescript-compile', function() {
+gulp.task('typescript-compile', function () {
 
     gulp.src(['src/ts/**/*.ts'])
         .pipe(typescript({
@@ -36,48 +36,48 @@ gulp.task('typescript-compile', function() {
             removeComments: true,
             sortOutput: true
         }))
-    .js
-    .pipe(concat("main.js"))
-    .pipe(gulp.dest('build/js/'));
+        .js
+        .pipe(concat("main.js"))
+        .pipe(gulp.dest('build/js/'));
 
 });
 
 gulp.task('clean', function (callback) {
-  rimraf('./build', callback);
+    rimraf('./build', callback);
 });
 
-gulp.task("clean-build",['clean'],function() {
+gulp.task("clean-build", ['clean'], function () {
     runSequence(
         "build-minify",
         "copy-all"
     );
 });
 
-gulp.task('copy-all', ['copy-libs','copy-html','copy-css','copy-texture']);
+gulp.task('copy-all', ['copy-libs', 'copy-html', 'copy-css', 'copy-texture']);
 
-gulp.task('copy-libs',function(){
-    gulp.src('src/libs/**/*.js').pipe(gulp.dest('build/js/')); 
+gulp.task('copy-libs', function () {
+    gulp.src('src/libs/**/*.js').pipe(gulp.dest('build/js/'));
 });
-gulp.task('copy-html',function(){
-    gulp.src('src/index.html').pipe(gulp.dest('build/')); 
+gulp.task('copy-html', function () {
+    gulp.src('src/index.html').pipe(gulp.dest('build/'));
 });
-gulp.task('copy-texture',function(){
-    gulp.src('src/texture/**/*.+(jpg|jpeg|png)').pipe(gulp.dest('build/texture/')); 
+gulp.task('copy-texture', function () {
+    gulp.src('src/texture/**/*.+(jpg|jpeg|png)').pipe(gulp.dest('build/texture/'));
 });
-gulp.task('copy-css',function(){
-    gulp.src('src/css/*.css').pipe(gulp.dest('build/css/')); 
+gulp.task('copy-css', function () {
+    gulp.src('src/css/*.css').pipe(gulp.dest('build/css/'));
 });
 
 // browserSync-browserSync-reload
-gulp.task("reload", function() {
-    browserSync.reload(); 
+gulp.task("reload", function () {
+    browserSync.reload();
 });
 
-gulp.task("watch",["reload"], function() {
+gulp.task("watch", ["reload"], function () {
     gulp.watch(['src/libs/**/*.js'], ['copy-libs']);
     gulp.watch(['src/index.html'], ['copy-html']);
-    gulp.watch(['src/texture/**/*.+(jpg|jpeg|png)'],['copy-texture']);
-    gulp.watch(['src/css/*.css'],['copy-css']);
-    gulp.watch(['src/ts/**/*.*'],['typescript-compile']);
-    gulp.watch(['build/**/*.*'],['reload']);
+    gulp.watch(['src/texture/**/*.+(jpg|jpeg|png)'], ['copy-texture']);
+    gulp.watch(['src/css/*.*'], ['copy-css']);
+    gulp.watch(['src/ts/**/*.*'], ['typescript-compile']);
+    gulp.watch(['build/**/*.*'], ['reload']);
 });
